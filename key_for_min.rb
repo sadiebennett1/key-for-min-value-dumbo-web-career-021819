e2fcddf0b4cb89e1587e5ab38024f319f6a0d9ba
blithe-hacker-0535 <sadiebenn1@gmail.com>
2019-02-12 17:54:21 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,20 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  arr = []
+  name_hash.collect do |first, second|
+    arr << second
+  end
+  i = arr[0]
+  arr.each do |num|
+    if num <= i 
+      i = num
+    end
+  end
+  name_hash.each do |first, second|
+    if second == i
+      return first
+    end
+  end
 
 end
