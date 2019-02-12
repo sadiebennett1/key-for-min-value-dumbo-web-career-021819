a9ccf8ba46b621448b8a0d9e3c9310b4879b88e6
blithe-hacker-0535 <sadiebenn1@gmail.com>
2019-02-12 17:55:05 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,23 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  if name_hash.length == 0
+    return nil
+  end
+  arr = []
+  name_hash.collect do |first, second|
+    arr << second
+  end
+  i = arr[0]
+  arr.each do |num|
+    if num <= i
+      i = num
+    end
+  end
+  name_hash.each do |first, second|
+    if second == i
+      return first
+    end
+  end
 
 end
